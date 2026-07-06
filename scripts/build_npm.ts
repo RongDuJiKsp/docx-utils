9a05bee36d01e82340ef9d8555c39c89ad7b16b9
yuqizhou <yuqizhou@tencent.com>
2026-07-06 15:07:54 +0800

    feat: pub to npm

---
+++ scripts/build_npm.ts
@@ -0,0 +1,37 @@
+import { build, emptyDir } from '@deno/dnt'
+
+// 动态解析 deno.json
+const denoJson = JSON.parse(await Deno.readTextFile('./deno.json'))
+const { name, version, license } = denoJson as {
+  name: string
+  version: string
+  license: string
+}
+
+
+await emptyDir('./npm')
+
+await build({
+  entryPoints: ['./index.ts'],
+  outDir: './npm',
+  shims: {
+    deno: true,
+  },
+  package: {
+    name,
+    version: Deno.args[0] || version,
+    description: 'DOCX 文本工具集，支持文档解析、图表引用查找、AI 审查等功能',
+    license,
+    repository: {
+      type: 'git',
+      url: 'git+https://github.com/rdjksp/docx-utils.git',
+    },
+    bin: {
+      'docx-utils': './esm/index.js',
+    },
+  },
+  test: false,
+  postBuild() {
+    Deno.copyFileSync('README.md', 'npm/README.md')
+  },
+})
